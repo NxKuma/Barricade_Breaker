789702c9ad911576f7be8d2f75eb65484d374ac4
NxKuma <55661040+NxKuma@users.noreply.github.com>
2021-07-22 22:37:32 +0800

    Fixed Character Selection Focus

--- Characters/GDScript/Character Selectoin.gd
+++ Characters/GDScript/Character Selectoin.gd
@@ -6,7 +6,28 @@ var buttons = []
 func _ready():
 	for child in get_children():
 		buttons.append(child)
+	
+	for button in buttons:
+		print(button.is_clicked)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _physics_process(delta):
-	pass
+
+func focus(object: Sprite):
+	var button_focus = object
+	
+	for button in buttons:
+		if button == button_focus:
+			if button.is_clicked:
+				button.focus_value += 10
+		else:
+			button.is_clicked = false
+			button.char_match()
+			
+
+#func _physics_process(delta):
+#	for button in buttons:
+#		if button.is_clicked:
+#			button.focus_value += 10
+		
+		
+		print(button.focus_value)
+
--- Misc/UI/Selection.gd
+++ Misc/UI/Selection.gd
@@ -2,26 +2,36 @@ extends Sprite
 
 export(int, "Gecko", "Crew", "Kiara", "Captain") var character = 0
 var anim_name
-
+var is_clicked = false
+var focus_value = 0
 onready var anim = get_node("AnimationPlayer")
+onready var parent = self.get_parent()
 
 
+func char_match():
+	if !is_clicked:
+		match character:
+			0:
+				frame = 1
+			1:
+				frame = 3
+			2:
+				frame = 5
+			3:
+				frame = 7
+	print(is_clicked)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	material.set_shader_param("flash_modifier", 0)
-	match character:
-		0:
-			frame = 1
-		1:
-			frame = 3
-		2:
-			frame = 5
-		3:
-			frame = 7
+	char_match()
 
 func _input(event):
 	if event is InputEventMouseButton and event.button_index == BUTTON_LEFT:
 		if get_rect().has_point(get_local_mouse_position()):
 			anim.play("Flash_"+str(character))
+			is_clicked = true
+			focus_value += 10
+			parent.focus(self)
+			
 
